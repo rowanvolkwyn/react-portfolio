36ce76470964e64585256b5a34c4570ee4db3e85
Rowan <volkwyn.rowan@gmail.com>
2023-04-02 20:05:17 +1000

    worked on landing page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,6 +1,7 @@
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import SocialMediaIcons from "../components/SocialMediaIcons"
 
 const Landing = ({ setSelectedPage }) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -27,8 +28,85 @@ const Landing = ({ setSelectedPage }) => {
                         />
                     </div>
                 ) : (
-                    <div></div>
+                    <img 
+                        alt="profile"
+                        className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full
+                        max-w-[400px] md:max-w-[600px]"
+                        src="assets/profile-image.png"
+                    />
                 )}
+            </div>
+
+            {/* MAIN SECTION */}
+            <div className="z-30 basis 2/5 mt:12 md:mt-32">
+                {/* HEADING */}
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}                        transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50},
+                        visible: { opacity: 1, x: 0}
+                    }}
+                >
+                    <p className="text-6xl font-playfair z-10 text-center md:text-start">                            Rowan {""}
+                        <span
+                            className="xs:relative xs:text-deep-blue xs:font-semibold z-20
+                            xs:before:content-brush before:absolute before:-left-[40px]
+                            before:-top-[120px] before:z-[-1]"
+                        >
+                            Volkwyn
+                        </span>
+                    </p>
+                    <p className="mt-10 mb-7 text-sm text-center md:text-start"></p>
+                </motion.div>
+
+                {/* CALL TO ACTIONS */}
+                <motion.div
+                    className="flex mt-5 justify-center md:justify-start"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}                        transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50},
+                        visible: { opacity: 1, x: 0}
+                    }}
+                >
+                    <AnchorLink
+                        className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7
+                        font-semibold hover:bg-blue hover:text-white transition duration-500"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        Contact Me
+                    </AnchorLink>
+
+                    <AnchorLink
+                        className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        <div className="bg-deep-blue hover:text-red transition duration-500 w-full
+                        h-full flex items-center justify-center font-playfair px-10">
+                            Let's talk.
+                        </div>
+                    </AnchorLink>
+                </motion.div>
+
+                <motion.div
+                    className="flex mt-5 justify-center md:justify-start"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.4, duration: 0.5 }}                        transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50},
+                        visible: { opacity: 1, x: 0}
+                    }}
+                >
+                    <SocialMediaIcons />
+                </motion.div>
             </div>
         </section>
     )
